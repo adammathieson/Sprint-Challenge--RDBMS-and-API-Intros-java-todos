b3cc07b48fc22f632999f9b7e3f03d1ea40b3ff5
Adam Mathieson <mathieson1222@gmail.com>
2019-07-19 18:58:03 -0700

    todos/todo/{todoid} updates todo

--- src/main/java/com/lambdaschool/todos/controller/TodosController.java
+++ src/main/java/com/lambdaschool/todos/controller/TodosController.java
@@ -1,11 +1,25 @@
 package com.lambdaschool.todos.controller;
 
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.lambdaschool.todos.model.Todo;
+import com.lambdaschool.todos.service.TodoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/todos")
 public class TodosController
 {
+    @Autowired
+    private TodoService todoService;
+
+    // update todo by todoid
+    @PutMapping(value = "/todo/{todoid}", consumes = {"application/json"}, produces = {"application/json"})
+    public ResponseEntity<?> updateTodo(@RequestBody Todo todo, @PathVariable long todoid)
+    {
+        todoService.update(todo, todoid);
+        return new ResponseEntity<>(todo, HttpStatus.OK);
+    }
 }
