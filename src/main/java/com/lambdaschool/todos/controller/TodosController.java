798e9e853374160c7e393762c73f801dece3df16
Adam Mathieson <mathieson1222@gmail.com>
2019-07-19 19:25:31 -0700

    refactor todo/{todoid} to only update signed in user

--- src/main/java/com/lambdaschool/todos/controller/TodosController.java
+++ src/main/java/com/lambdaschool/todos/controller/TodosController.java
@@ -3,9 +3,12 @@ package com.lambdaschool.todos.controller;
 
 import com.lambdaschool.todos.model.Todo;
 import com.lambdaschool.todos.service.TodoService;
+import com.lambdaschool.todos.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -15,10 +18,15 @@ public class TodosController
     @Autowired
     private TodoService todoService;
 
+    @Autowired
+    private UserService userService;
+
     // update todo by todoid
     @PutMapping(value = "/todo/{todoid}", consumes = {"application/json"}, produces = {"application/json"})
     public ResponseEntity<?> updateTodo(@RequestBody Todo todo, @PathVariable long todoid)
     {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        todo.setUser(userService.findUserByName(authentication.getName()));
         todoService.update(todo, todoid);
         return new ResponseEntity<>(todo, HttpStatus.OK);
     }
